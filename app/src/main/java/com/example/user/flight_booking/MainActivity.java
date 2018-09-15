b23c7c77f4dafe401c70404b639fe143bb1eb420
Sana Ben Hammamia <benhammamiasana@gmail.com>
2018-09-15 16:10:40 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/user/flight_booking/MainActivity.java
+++ app/src/main/java/com/example/user/flight_booking/MainActivity.java
@@ -37,10 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent1);
                 return true;
 
-            case R.id.menu_dest:
-            Intent intent2 = new Intent(this, Destinations.class);
-                startActivity(intent2);
-                return true;
+           
             case R.id.menu_book:
             Intent intent3 = new Intent(this, Book.class);
                 startActivity(intent3);
